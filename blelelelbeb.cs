beae68aeef79cbc73a82ebd48ba3741510fbb55e
martianK3jC <kceniza24@gmail.com>
2025-09-03 18:47:09 +0800

    Current Progress: OOP pa

---
+++ blelelelbeb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPrac
+{
+    internal class blelelelbeb
+    {
+        /knlfsnaasknfknaspfnaspfnas;kfns
+
+            asfm'aslfmaslmf
+            asmf
+            asmf
+            aslmf
+            samf
+            asmf
+            as
+    }
+}
